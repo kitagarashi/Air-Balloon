bdf4f8b04f003406831e9608ef91d3d1a022af34
kitagarashi <ilya.smolenskiy.00@gmail.com>
2025-03-10 16:15:00 +0300

    fdgfd
    fdgfd

---
+++ Assets/_Project/Scripts/BuildScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript
+{
+    public static void PerformBuild()
+    {
+        // Настройка пакета и SDK
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, Environment.GetEnvironmentVariable("PACKAGE_NAME"));
+        PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel23;
+        PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel34;
+
+        // Конфигурация сборки
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/_Project/Scenes/Game.unity" }, // Укажите свои сцены
+            locationPathName = "build/app.apk",
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(options);
+    }
+}
